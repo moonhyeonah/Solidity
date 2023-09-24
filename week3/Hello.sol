81c4df9dc6b46b2df0d48f4883fade266f2d7907
moonhyeonah <moonhyeonah@gmail.com>
2023-09-24 12:32:16 +0900

    Update Hello.sol

--- week3/Hello.sol
+++ week3/Hello.sol
@@ -1,9 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.7;
+pragma solidity >=0.7.0 <0.9.0;
 
-contract Ex2_1 {    
-    //행 단위 주석
-    /*
-        블록 단위 주석
-    */
+contract Counter{
+
+    uint count;
+    string test;
+    bytes32 test2;
+    constructor() {
+        count = 0;
+        test = "Hello World";
+        test2 = "Hello2";
+    }
+
+    function getCount() public view returns(uint){
+        return count;
+    }
+
+    function getbytes() public view returns(string memory){
+        return test;
+    }
+
+    function getstring() public view returns(bytes32){
+        return test2;
+    }
+    function incrementCount() public{
+        count = count + 1;
+    }
 }
