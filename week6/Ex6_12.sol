aecc3b646e49043338f61bedbb0cb481e420bf0a
moonhyeonah <moonhyeonah@gmail.com>
2025-10-12 11:31:19 +0900

    Week6

---
+++ week6/Ex6_12.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Ex6_12 {
+ 
+    function getBalance(address _address) public view returns(uint) {
+        return _address.balance;
+    }
+    function getMsgValue() public payable returns(uint) {
+        return msg.value;
+    }
+     
+    function getMsgSender() public view returns(address) {
+        return msg.sender;
+    }
+}
---
+++ week6/in/Ex6_10_2.sol
@@ -0,0 +1,6 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+contract PartTimer {
+    string public workPlace = "A pizza shop";
+    uint public workingHours = 6;
+}
